5a01993535a2e7833e89fe25547b940992dcdd6a
Steve Castaneda <steve.castaneda@me.com>
2026-09-24 16:02:58 -0500

    Publish build from 999c714

--- changelog.js
+++ changelog.js
@@ -7,7 +7,8 @@ window.WILDS_CHANGELOG = [
       'Chop down forest trees! A sword takes three swings, and an axe fells a tree in one. Stumps stay behind.',
       'Crack woodland rocks for stone. Jellies drop gel, skeletons drop bone, and Hollowoak leaves a big pile of wood.',
       'Materials go in a pouch that you keep even after a run ends. Check it at the bottom of your Inventory.',
-      'Craft at the workbench in the cottage: an axe, a spear, bone daggers, a bark shield, weapon sharpening, and healing salves.'
+      'Craft at the workbench in the cottage: an axe, a spear, bone daggers, a bark shield, weapon sharpening, and healing salves.',
+      'New recipe: brew Health of the Hearts, a potion from the depths of Mt. Doom.'
     ]
   },
   {
